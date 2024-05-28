40b90f4c451d8c20b17b76c1f118fd0af760ea08
Delaney Green <delaney.rosa1@gmail.com>
2024-05-28 11:15:16 -0500

    first commit to main

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloASPDotNET;
+using Microsoft.AspNetCore.Mvc;
+
+
+public class HelloController : Controller
+{
+
+    //GET: <port>/helloworld/
+   [HttpGet]
+   [Route("/helloworld")]
+    public IActionResult Index()
+    {
+        string html = "<form method='post' action='/helloworld/welcome'>" +
+         "<input type='text' name='name' />" +
+         "<input type='submit' value='Greet Me!' />" +
+         "</form>";
+        return Content(html, "text/html");
+    }
+
+//GET: <port>/hello/welcome
+    [HttpPost]
+    [Route("/helloworld/welcome")]
+    public IActionResult Welcome(string name = "World")
+    {
+        return Content("<h1>Welcome to my app, "+name+"!</h1>", "text/html");
+    }
+
+}
