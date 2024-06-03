b70a87f739b2cea9f98607f0ec3b86638bfaef7a
Delaney Green <delaney.rosa1@gmail.com>
2024-06-03 09:52:11 -0500

    Stupid stupid not working stupid

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -3,25 +3,15 @@
 using System.ComponentModel.Design;
 using Microsoft.AspNetCore.Mvc;
 
-[Route("/helloworld")]
 public class HelloController : Controller
 {
 
-    //GET: <port>/helloworld/
+
       [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post'>" +
-                "<input type='text' name='name' />" +
-                "<select name='language'><option value='english' selected>English</option>" +
-                "<option value='spanish'>Spanish</spanish>" +
-                "<option value='bosnian'>Bosnian</option>" +
-                "<option value='vietnamese'>Vietnamese</option>" +
-                "<option value='french'>French</option></select>" +
-                "<input type='submit' value='Greet Me!'/>" +
-                "</form>";
-
-            return Content(html, "text/html");
+        
+            return View();
 
         }
 
